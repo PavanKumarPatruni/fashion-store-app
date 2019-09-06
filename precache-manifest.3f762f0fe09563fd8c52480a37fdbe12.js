ca8dccb1caa3a84d22808a4f4ef9e712ae219713
Pavan Kumar Patruni <>
2019-09-06 18:51:37 +0530

    Updates

---
+++ precache-manifest.3f762f0fe09563fd8c52480a37fdbe12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202f49955006cb279f821bde3e2adb35",
+    "url": "/fashion-store-app/index.html"
+  },
+  {
+    "revision": "6b6a93a91364ad68fdfd",
+    "url": "/fashion-store-app/static/css/main.487eb347.chunk.css"
+  },
+  {
+    "revision": "60c112f153e6e6fc2409",
+    "url": "/fashion-store-app/static/js/2.977f6867.chunk.js"
+  },
+  {
+    "revision": "6b6a93a91364ad68fdfd",
+    "url": "/fashion-store-app/static/js/main.747b3b90.chunk.js"
+  },
+  {
+    "revision": "4cd23c6d52a9eb5a6c9c",
+    "url": "/fashion-store-app/static/js/runtime~main.399020c1.js"
+  }
+]);
